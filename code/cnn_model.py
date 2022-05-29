94e1f968af663b5f18e4a2008cbffd16d6e7f575
angus <angusyang.cs09@nycu.edu.tw>
2022-05-30 02:18:11 +0800

    add: cnn model

--- code/cnn_model.py
+++ code/cnn_model.py
@@ -9,3 +9,37 @@ from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
 from keras import backend as K
+
+def CNN():
+    image_x = 50
+    image_y = 50
+    num_of_class = 50
+
+    model = Sequential()
+
+    #layer 1
+    model.add(Conv2D(32, (3, 3), input_shape=(image_x, image_y, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Dropout(0.2))
+
+    #layer 2
+    model.add(Conv2D(64, (3, 3), input_shape=(image_x, image_y, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Dropout(0.2))
+
+    #layer 3
+    model.add(Conv2D(128, (3, 3), input_shape=(image_x, image_y, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Dropout(0.2))
+
+    #flatten
+    model.add(Flatten())
+
+    #output layer
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(num_of_class, activation='softmax'))
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    model.summary()
