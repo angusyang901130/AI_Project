cda0309d6c6d8fd0398dbb059ff42ff1f850e894
angus <angusyang.cs09@nycu.edu.tw>
2022-06-01 00:24:11 +0800

    init: train

--- code/cnn.py
+++ code/cnn.py
@@ -48,12 +48,32 @@ def CNN(image_x, image_y, num_of_class):
         metrics = ['accuracy']
     ) """
 
-    model.summary()
-
-    filepath = 'cnn_model.h5'
+    filepath = 'cnn.h5'
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     callback_list = [checkpoint]
     
     return model, callback_list
 
-
+def train(image_x, image_y, num_of_class):
+    
+    with open('train_images', 'rb') as f:
+        train_images = np.array(pickle.load(f))
+    with open('train_labels', 'rb') as f:
+        train_labels = np.array(pickle.load(f))
+    with open('validate_images', 'rb') as f:
+        validate_images = np.array(pickle.load(f))
+    with open('validate_labels', 'rb') as f:
+        validate_labels = np.array(pickle.load(f))
+    
+    train_images = np.reshape(train_images, (train_images.shape[0], image_x, image_y, 1))
+    validate_images = np.reshape(validate_images, (validate_images.shape[0], image_x, image_y, 1))
+    # transfer integer to binary encoding, example: num_of_class = 3, 2 => [0,0,1], 1 => [0,1,0]
+    train_labels = np_utils.to_categorical(train_labels)
+    validate_labels = np_utils.to_categorical(validate_labels)
+    
+    model, callback_list = CNN(image_x, image_y, num_of_class)
+    model.summary()
+    model.fit(train_images, train_labels, validation_data=(validate_images, validate_labels), epochs=50, batch_size=500, callbacks=callback_list)
+    scores = model.evaluate(validate_images, validate_labels, verbose=0)
+    print("CNN Error: %.2f%%" % (100-scores[1]*100))
+    model.save('cnn.h5')
